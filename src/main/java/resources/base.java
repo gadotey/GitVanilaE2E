8832a4ac4a00a45e4ad8bc529aec3eb0f350e55c
Glenn <gadotey@outlook.com>
2020-01-14 22:30:04 -0500

    first commit

---
+++ src/main/java/resources/base.java
@@ -0,0 +1,74 @@
+package resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class base {
+
+	public static WebDriver driver;
+	public Properties prop;
+	public WebDriver initializeDriver() throws IOException
+	{
+
+		prop= new Properties();
+		//System.getProperty("user.dir")
+		FileInputStream fis=new FileInputStream(System.getProperty("user.dir") + "\\src\\main\\java\\resources\\data.properties");
+
+		prop.load(fis);
+		
+		//String browserName=prop.getProperty("browser");
+		String browserName = System.getProperty("browser");
+		System.out.println(browserName);
+
+		if(browserName.contains("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Drivers\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			if(browserName.contains("headless"));
+			{
+				options.addArguments("headless");
+			}
+			driver= new ChromeDriver(options);
+			//execute in chrome driver
+
+		}
+		else if (browserName.equals("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\Drivers\\geckodriver.exe"); 
+			driver= new FirefoxDriver();
+			//firefox code
+		}
+		else if (browserName.equals("IE"))
+		{
+			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + "\\Drivers\\IEDriverServer.exe"); 
+			driver= new InternetExplorerDriver();
+			//	IE code
+		}
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+
+
+	}
+
+	public void getScreenshot(String result) throws IOException
+	{
+		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C://test//"+result+"screenshot.png"));
+
+	}
+
+
+}
